97ba5f3b2a18a014148f9715616353a6d6d48fdc
Avinash <aviku9911@gmail.com>
2026-09-09 09:01:49 +0530

    added some dcomments to make the code more understandable

--- Langchain/Lecture_4/chatbot.py
+++ Langchain/Lecture_4/chatbot.py
@@ -1,3 +1,10 @@
+# This a simple chatbot withoud conversation context i.e this bot won't have the chat history, SO
+# we can't ask somthing related to the previous chat.
+# e.g.:
+# AI :  The sum of 2 and 0 is **2**.
+# You : multiply this with 10
+# AI :  It looks like you didn't include the number or problem! Please provide what you would like me to multiply by 10, and I'll give you the answer.
+
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 
